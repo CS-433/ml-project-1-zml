a00b7b9c1361a64be89d0bb98e5c8e492adaac73
Lemmy00 <64690721+Lemmy00@users.noreply.github.com>
2022-10-24 01:44:47 +0200

    Sad..

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-# loss function
+
 def mean_square_error(y, tx, w):
     """Calculate the loss using either MSE or MAE.
 
@@ -27,15 +28,15 @@ def linear_regression_gradient(y, tx, w):
     Returns:
         An array of shape (D, ) (same shape as w), containing the gradient of the loss at w.
     """
-    return  -tx.T @ (y - tx @ w) / y.shape[0]
+    return -tx.T @ (y - tx @ w) / y.shape[0]
 
 
 def sigmoid(x):
     """Computes the value of the sigmoid function on x.
-    
+
     Args:
         x: scalar or numpy array
-    
+
     Returns:
         A scalar or an array of shape (N, ) of respective values of the sigmoid function.
     """
@@ -56,16 +57,18 @@ def cross_entropy_loss(y, tx, w, lambda_=0, bias_term=False):
         a non-negative loss
     """
     n = y.shape[0]
+    eps = 10e-10
+
     start_2_norm = 1 if bias_term else 0
-    
+
     sigmoid_values = sigmoid(tx @ w)
-    return -(y.T @ np.log(sigmoid_values) + (1 - y.T) @ np.log(
-        1 - sigmoid_values)) / n + lambda_ / n * np.linalg.norm(w[start_2_norm: ], 2)
+    return -(y.T @ np.log(sigmoid_values + eps) + (1 - y.T) @ np.log(
+        1 - sigmoid_values + eps)) / n + lambda_ / n * np.linalg.norm(w[start_2_norm:], 2)
 
 
 def logistic_regression_gradient(y, tx, w, lambda_=0, bias_term=False):
     """Compute the gradient of loss for logistic regression.
-    
+
     Args:
         y:  shape=(N, 1)
         tx: shape=(N, D)
@@ -77,7 +80,7 @@ def logistic_regression_gradient(y, tx, w, lambda_=0, bias_term=False):
     """
     n, d = tx.shape
     ridge_matrix = np.eye(d)
-    
+
     if bias_term:
         ridge_matrix[0, 0] = 0
 
@@ -87,25 +90,25 @@ def logistic_regression_gradient(y, tx, w, lambda_=0, bias_term=False):
 def split_data(x, y, ratio):
     """
     Split the dataset based on the split ratio.
-    
+
     Args:
         x: numpy array of shape (N, D), N is the number of samples.
         y: numpy array of shape (N,).
         ratio: scalar in [0,1]
-        
+
     Returns:
         x_tr: numpy array containing the train data.
         x_te: numpy array containing the test data.
         y_tr: numpy array containing the train labels.
         y_te: numpy array containing the test labels.
     """
-    N = x.shape[0]
-    div = int(N * ratio)
-    
-    perm = np.random.permutation(N)
-    
+    n = x.shape[0]
+    div = int(n * ratio)
+
+    perm = np.random.permutation(n)
+
     perm_tr, perm_te = perm[:div], perm[div:]
-    
+
     return x[perm_tr], x[perm_te], y[perm_tr], y[perm_te]
 
 
@@ -116,6 +119,11 @@ def predictions(x, weights):
         return 0
 
 
+def compute_score(y, tx, weights, f=predictions):
+    y_pred = np.array([f(x, weights) for x in tx])
+    return (y_pred == y).sum() / len(y)
+
+
 def standardize(x):
     """Standardize the original data set."""
     mean_x = np.mean(x)
@@ -124,3 +132,22 @@ def standardize(x):
     x = x / std_x
     return x, mean_x, std_x
 
+
+def add_bias_term(x):
+    n = x.shape[0]
+    return np.concatenate((np.ones((n, 1)), x), axis=1)
+
+
+def kfolds(n, kfold=10, shuffle=True):
+    if shuffle:
+        perm = np.random.permutation(n)
+    else:
+        perm = np.arange(n)
+
+    kfoldsShuffle, div = [], int(kfold / 100 * n)
+    for i in range(kfold):
+        test_indices = perm[div * i: div * (i + 1)]
+        train_indices = np.concatenate((perm[:div * i], perm[div * (i + 1):]))
+        kfoldsShuffle.append((train_indices, test_indices))
+
+    return kfoldsShuffle

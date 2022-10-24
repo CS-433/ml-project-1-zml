ab2df03b9fc5dcb666b6e54d0f1218ce12082630
Yurui <alexzyr@163.com>
2022-10-24 22:29:46 +0200

    add remove outliers

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 # loss function
 def mean_square_error(y, tx, w):
     """Calculate the loss using either MSE or MAE.
@@ -27,7 +28,7 @@ def linear_regression_gradient(y, tx, w):
     Returns:
         An array of shape (D, ) (same shape as w), containing the gradient of the loss at w.
     """
-    return  -tx.T @ (y - tx @ w) / y.shape[0]
+    return -tx.T @ (y - tx @ w) / y.shape[0]
 
 
 def sigmoid(x):
@@ -57,10 +58,10 @@ def cross_entropy_loss(y, tx, w, lambda_=0, bias_term=False):
     """
     n = y.shape[0]
     start_2_norm = 1 if bias_term else 0
-    
+
     sigmoid_values = sigmoid(tx @ w)
     return -(y.T @ np.log(sigmoid_values) + (1 - y.T) @ np.log(
-        1 - sigmoid_values)) / n + lambda_ / n * np.linalg.norm(w[start_2_norm: ], 2)
+        1 - sigmoid_values)) / n + lambda_ / n * np.linalg.norm(w[start_2_norm:], 2)
 
 
 def logistic_regression_gradient(y, tx, w, lambda_=0, bias_term=False):
@@ -77,7 +78,7 @@ def logistic_regression_gradient(y, tx, w, lambda_=0, bias_term=False):
     """
     n, d = tx.shape
     ridge_matrix = np.eye(d)
-    
+
     if bias_term:
         ridge_matrix[0, 0] = 0
 
@@ -101,11 +102,11 @@ def split_data(x, y, ratio):
     """
     N = x.shape[0]
     div = int(N * ratio)
-    
+
     perm = np.random.permutation(N)
-    
+
     perm_tr, perm_te = perm[:div], perm[div:]
-    
+
     return x[perm_tr], x[perm_te], y[perm_tr], y[perm_te]
 
 
@@ -124,3 +125,34 @@ def standardize(x):
     x = x / std_x
     return x, mean_x, std_x
 
+
+def accuracy(y, y_hat):
+    """Calculate the accuracy"""
+    return (y == y_hat).sum() / y.shape[0]
+
+
+def outliers_map(x):
+    """get outliers position per column"""
+    p3, p1 = np.percentile(x[(x != -999)], [95, 5])
+    iqr = p3 - p1
+    low = p1 - 1.5 * iqr
+    high = p3 + 1.5 * iqr
+    # print(low, high)
+    mask1 = (x < high) & (x > low)
+    mask2 = (x == -999)
+    return mask1 | mask2
+
+
+def remove_outliers(x, y):
+    """remove outliers"""
+    x_copy = x.copy()
+    y_copy = y.copy()
+    outliers = np.ones(x.shape, dtype=bool)
+
+    for i in range(x.shape[1]):
+        outliers[:, i] = outliers_map(x[:, i])
+    outliers = (np.sum(outliers, axis=1) == 30)
+    x_copy = x_copy[outliers]
+    y_copy = y_copy[outliers]
+
+    return x_copy, y_copy
